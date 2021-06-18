5331d3c2958c69dcf5608b17a0f4c748de603147
Thiago Amorim <64905942+XaVeZ-cmd@users.noreply.github.com>
2021-06-18 12:47:28 -0300

    Update StockController.java

--- src/main/java/com/project/bootcamp/controller/StockController.java
+++ src/main/java/com/project/bootcamp/controller/StockController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-/*@CrossOrigin*/
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/stock")
 public class StockController {
